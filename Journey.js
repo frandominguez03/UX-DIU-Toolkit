3bbd9cc45140ddbb2b48e46eb3f0f4ccae37d7f6
Francisco Domínguez <d3vcho@protonmail.com>
2020-03-04 16:55:29 +0100

    Journey Maps

--- Journey.js
+++ Journey.js
@@ -30,54 +30,54 @@ angular.module("angular", [])
                 /*************************************/
                 
 				Id: 0,
-				Name: "Juan Lopez",
+				Name: "José Luis Pomares",
                 Photo: "man.png",
     
                 /*** PASO #1: INSPIRACION ***/ 
-                goal1: "quiere preparar un viaje con sus amigos en Semana Santa",
-                touch1: "agenda",
+                goal1: "Quiere hacer un viaje en las vacaciones de Semana Santa",
+                touch1: "Calendario del móvil",
                 feel1: "4",
-                con1: "ver cuantos días puede tener libres para organizar lugar de viaje ",
+                con1: "No tiene acompañantes para el viaje",
                 ima1: "cartoon-planning.png",
 				
                 /*** PASO #2: DECICION ***/ 
-                goal2: "Busca en internet ofertas para esas fechas",
-                touch2: "ordenador",
+                goal2: "Busca en internet posibles opciones para hacer viajes",
+                touch2: "Móvil",
                 feel2: "2",
-                con2: "hay demasiada información y pierde mucho tiempo, no hay precios 'baratos'",
+                con2: "No hay muchas opciones ni aplicaciones para lo que José Luis quiere",
                 ima2: "cartoon-PCangry.png",
                 
                 /*** PASO #3: ACTUA ***/ 
                 
-                goal3: "Decide buscar un alojamiento rural  en plasencia, donde hay procesiones y parece que hará buen tiempo",
-                touch3: "móvil (el tiempo)",
-                feel3: "3",
-                con3: "Está preocupado por el tiempo y el desplazamiento (coche y aparcamiento)",
+                goal3: "Encuentra Pazifiko, se registra y encuentra determinadas opciones",
+                touch3: "Móvil",
+                feel3: "4",
+                con3: "Varias de las opciones no incluyen gran cantidad de fiestas",
                 ima3: "cartoon-phone.png",
                 
                 /*** PASO #4: OBSERVA ***/ 
                 
-                goal4: "Los amigos le recomiendan una página para escoger alojamientos",
-                touch4: "ordenador",
-                feel4: "4",
-                con4: "Buscar opciones en el lugar que había seleccionado, viendo precios y distancias, tiene que ver si hay aparcamiento fácil",
+                goal4: "Observa los viajes propuestos",
+                touch4: "Móvil (WhatsApp)",
+                feel4: "3",
+                con4: "No le convence ningún viaje de los propuestos y decide organizarlo él",
                 ima4: "cartoon-PCtyping.png",
                 
                  /*** PASO #5: ANALIZA ***/ 
                 
-                goal5: "Se encuentra 3 opciones que encajan en sus preferencias",
-                touch5: "móvil (whatsapp)",
-                feel5: "2",
-                con5: "Llama a sus amigos (whatsapp no responen) para ver cual es su preferencia, tienen que reservar rápido por los precios",
+                goal5: "A través de la plataforma crea un viaje a Punta Cana",
+                touch5: "Móvil (WhatsApp)",
+                feel5: "4",
+                con5: "Recibe pocas propuestas de compañeros",
                 ima5: "cartoon-phoning.png",
                 
                 
                 /*** PASO #6: CONCLUSION ***/ 
                 
-                goal6: "Consigue reservar, otro año se encarga otro!",
-                touch6: "ordenador",
-                feel6: "3",
-                con6: "algunos amigos no confirmaron por lo que tuvo que seleccionar reserva con posibilidad de cancelación",
+                goal6: "El viaje a Punta Cana queda fijado con otros 3 compañeros de viaje que tienen los mismos gustos",
+                touch6: "Móvil (WhatsApp)",
+                feel6: "5",
+                con6: "No puede proponer un viaje a corto plazo ya que la plataforma no es muy conocida y no se reciben muchas respuestas",
                 ima6: "cartoon-resting.png",
                 
 			},
@@ -88,7 +88,7 @@ angular.module("angular", [])
                 /*************************************/
                 
 				Id: 1,
-				Name: "Monica Suarez",
+				Name: "María Teresa Lozano",
                 Photo: "woman.png",
                 
 				 /*** PASO #1: INSPIRACION ***/ 
@@ -101,13 +101,13 @@ angular.module("angular", [])
                 /*** PASO #2: DECICION ***/ 
                 goal2: "Ir a una agencia de viajes, y decirle sus preferencias y planes",
                 touch2: "Servicio (agencia)",
-                feel2: "4",
+                feel2: "3",
                 con2: "Tiene que desplazarse a agencia, explica su intenciones, le llamaran porque no hay nada interesante",
                 ima2: "cartoon-teamthinking.png",
                 
                 /*** PASO #3: ACTUA ***/ 
                 
-                goal3: "Le llaman a los pocos días con un viaje que no le convence",
+                goal3: "Las opciones ofrecidas por la agencia no se adecuan a las preferencias de la familia",
                 touch3: "Móvil (llamada)",
                 feel3: "2",
                 con3: "Piensa que ha perdido el tiempo",
@@ -115,27 +115,27 @@ angular.module("angular", [])
                 
                 /*** PASO #4: OBSERVA ***/ 
                 
-                goal4: "Busca una oferta en hoteles cerca de playa y con parque atracciones",
+                goal4: "Una conocida le comenta que usó una plataforma hace 3 semanas para un viaje próximo",
                 touch4: "Móvil (webapp)",
-                feel4: "2",
-                con4: "No hay mucha información del alojamiento ni de lo que hay alrededor, aunque el precio está bien, va por la calle por lo que está incómoda",
+                feel4: "3",
+                con4: "La plataforma no es muy conocida y no está muy segura de que vaya a funcionar pero le da una oportunidad",
                 ima4: "cartoon-phone-street.png",
                 
                  /*** PASO #5: ANALIZA ***/ 
                 
-                goal5: "Reserva a traves de la aplicación ",
+                goal5: "Consigue hacer una reserva con otro matrimonio de sus gustos similares para un viaje a Egipto",
                 touch5: "Móvil (webapp)",
                 feel5: "3",
-                con5: "Le pide muchos datos y le resulta incómodo completar formulario",
+                con5: "Sabe poco del otro matrimonio y no saben si su hijo se sentirá agusto",
                 ima5: "cartoon-phone-sitting.png",
 
                 
                 /*** PASO #6: CONCLUSION ***/ 
                 
-                goal6: "Consiguie reservar para vacaciones pero no era lo que tenía en mente",
-                touch6: "Ordenador (reserva OK)",
-                feel6: "2",
-                con6: "Tendrá que buscar más información del lugar para ver que actividades ofrece y donde aparacar!",
+                goal6: "El viaje está planeado pero la experiencia con la plataforma no ha sido la mejor",
+                touch6: "Móvil (reserva OK)",
+                feel6: "4",
+                con6: "Muchos datos por rellenar para que luego no salgan una gran cantidad de opciones a elegir",
                 ima6: "cartoon-PChard.png",
                 
                 
